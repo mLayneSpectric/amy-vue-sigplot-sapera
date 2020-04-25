804871764111931a329fdd5a64c75e55a246bb15
Michael Recachinas <mgr3yp@virginia.edu>
2020-04-25 13:50:25 +0100

    mod: Incorporate all 3 layers in example App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
-    <SigPlot id="plot1">
-      <ArrayLayer v-bind:plotData="[1, 2, 3, 4]" />
-      <ArrayLayer v-bind:plotData="[4, 3, 2, 1]" />
+    <SigPlot id="plot1" :plot-options="{ xi: !btnToggle }">
+      <ArrayLayer :plot-data="random" />
     </SigPlot>
-    <SigPlot id="plot2" v-bind:plot-options="{ xi: true }">
-      <ArrayLayer v-bind:plotData="[1, 2, 3, 4]" />
-      <ArrayLayer v-bind:plotData="[4, 3, 2, 1]" />
+    <SigPlot id="plot2">
+      <HrefLayer :href="hrefData" />
     </SigPlot>
     <SigPlot id="plot3">
-      <HrefLayer href="https://sigplot.lgsinnovations.com/dat/penny.prm" />
+      <PipeLayer :pipe-data="random" :options="{type: 2000, subsize: 1000}"/>
     </SigPlot>
+    <button id="toggler" @click="btnToggle = !btnToggle">Toggle Data</button>
   </div>
 </template>
 
@@ -18,13 +17,54 @@
 import SigPlot from "./components/SigPlot";
 import ArrayLayer from "./components/ArrayLayer";
 import HrefLayer from "./components/HrefLayer";
+import PipeLayer from "./components/PipeLayer";
 
 export default {
   name: "App",
   components: {
     SigPlot,
     ArrayLayer,
-    HrefLayer
+    HrefLayer,
+    PipeLayer
+  },
+  computed: {
+    hrefData() {
+      return this.btnToggle ? this.href1 : this.href2
+    },
+  },
+  data() {
+    return {
+      btnToggle: false,
+      href1: "https://sigplot.lgsinnovations.com/dat/penny.prm",
+      href2: "https://sigplot.lgsinnovations.com/dat/sin.tmp",
+      random: [],
+      random2D: [],
+      generateDataInterval: 0,
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.generateDataInterval)
+  },
+  mounted() {
+    this.generateData()
+  },
+  methods: {
+    generateData() {
+      this.generateDataInterval = setInterval(() => {
+        let random = [];
+        let random2D = [];
+        for (let i = 0; i < 1000; i += 1) {
+          random.push(Math.random());
+          let tmp = [];
+          for (let j = 0; j < 100; j += 1) {
+            tmp.push(Math.random());
+          }
+          random2D.push(tmp);
+        }
+        this.random = random;
+        this.random2D = random2D;
+      }, 16);
+    }
   }
 };
 </script>
@@ -43,5 +83,22 @@ export default {
   display: inline-block;
   height: 400px;
   width: 400px;
+    margin: 10px;
 }
+
+  #toggler {
+    height: 30px;
+    width: 100px;
+      display: block;
+    background: none;
+    border:1px solid gray;
+    border-radius: 3px;
+  }
+
+  #toggler:active {
+    box-shadow: inset 0 2px 3px 0 black;
+  }
+  #toggler:focus {
+    outline: none
+  }
 </style>
