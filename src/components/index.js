9ad1fc6f42ea2169de73fded49bcbdcf357f5a5d
Michael Recachinas <mgr3yp@virginia.edu>
2020-04-25 13:50:01 +0100

    mod: Register PipeLayer in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import SigPlot from "./SigPlot";
 import ArrayLayer from "./ArrayLayer";
 import HrefLayer from "./HrefLayer";
+import PipeLayer from "./PipeLayer";
 
 const Components = {
   SigPlot,
   ArrayLayer,
   HrefLayer,
+  PipeLayer,
 };
 
 Object.keys(Components).forEach((name) => {
